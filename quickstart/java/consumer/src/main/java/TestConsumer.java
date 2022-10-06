2ca71a96292ea34bf2b2205452bf150c4df8e63b
julienS <durieljulien@gmail.com>
2022-10-06 20:02:16 +0200

    Reproducing the application insight 3.4.1 issue AppInsight not tracking kafka consumer dependencies currently

--- quickstart/java/consumer/src/main/java/TestConsumer.java
+++ quickstart/java/consumer/src/main/java/TestConsumer.java
@@ -3,18 +3,27 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+
 public class TestConsumer {
+
+
     //Change constant to send messages to the desired topic
-    private final static String TOPIC = "test";
+    private final static String TOPIC = "eh-java-kafka-fc";
     
     private final static int NUM_THREADS = 1;
 
     public static void main(String... args) throws Exception {
 
+        // Logger logger = LoggerFactory.getLogger(TestConsumer.class);
+        // logger.info("Hello World");
         final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
 
         for (int i = 0; i < NUM_THREADS; i++){
             executorService.execute(new TestConsumerThread(TOPIC));
         }
+
+        System.out.println("eh-java-kafka-fc");
     }
 }
